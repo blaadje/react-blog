cba4ed220ae7f59578ff3fd2a28b67a425c076a8
Alexander <acharlot91@gmail.com>
2017-09-24 16:05:03 +0200

    adding firebase

---
+++ src/fire.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase'
+var config = {
+  apiKey: "AIzaSyCrz_5zMYSTgVFBKw7HhO1lQRMJhP_SRa4",
+  authDomain: "blog-d86fd.firebaseapp.com",
+  databaseURL: "https://blog-d86fd.firebaseio.com",
+  projectId: "blog-d86fd",
+  storageBucket: "blog-d86fd.appspot.com",
+  messagingSenderId: "496914051611"
+};
+firebase.initializeApp(config);
+export default fire;
